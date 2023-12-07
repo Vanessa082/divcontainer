ee288f417f6155911753864f1153009f4e758544
vanessa082 <wahvanessa22@gmail.com>
2023-12-07 10:54:11 +0100

    CORECTED LINTEERr linter

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const table = document.getElementById('table');
+const table = document.getElementById('table')
 
 const isPrimeNumber = (num) => {
-  if (num == 1) {
-    return true;;
+  if (num === 1) {
+    return true
   }
 
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
-      return false;;
+      return false
     }
   }
 
-  return true;;
+  return true
 }
 
 for (let i = 1; i <= 100; i++) {
-  let nodeEl;;
+  let nodeEl
 
-  if (i % 2 == 0) {
-    // even;
-    nodeEl = `<button style="background-color: green;">${i}</button>`;;
+  if (i % 2 === 0) {
+    // even
+    nodeEl = `<button style="background-color: green;">${i}</button>`
   } else if (isPrimeNumber(i)) {
-    // prime;
-    nodeEl = `<button style="background-color: red;">${i}</button>`;;
+    // prime
+    nodeEl = `<button style="background-color: red;">${i}</button>`
   } else {
-    // odd;
-    nodeEl = `<button style="background-color: yellow;">${i}</button>`;;
+    // odd
+    nodeEl = `<button style="background-color: yellow;">${i}</button>`
   }
 
-  table.innerHTML += nodeEl;;
+  table.innerHTML += nodeEl
 }
